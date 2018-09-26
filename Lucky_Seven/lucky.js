25a3c4b1ede7f07db4320164fd5235830450a150
TRamos5 <travis.ramos5@gmail.com>
2018-09-25 22:08:37 -0500

    restaurant 3

---
+++ Lucky_Seven/lucky.js
@@ -0,0 +1,6 @@
+
+//Adds functionality to "Play" button
+document.getElementById('playButton').addEventListener('click', function(){
+    var addMoney = document.getElementById('prompter');
+    addMoney.setAttribute('style', 'visibility: hidden');
+});
